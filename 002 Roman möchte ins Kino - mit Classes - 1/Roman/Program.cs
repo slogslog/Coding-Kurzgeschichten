8f334f89f129ee1be5b3b70817183775bc115fe5
slogslog <36892793+slogslog@users.noreply.github.com>
2019-03-08 22:30:33 +0100

    cleaned up solution of first part of Roman's story

--- 002 Roman möchte ins Kino - mit Classes - 1/Roman/Program.cs
+++ 002 Roman möchte ins Kino - mit Classes - 1/Roman/Program.cs
@@ -1,54 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Roman_möchte_ins_Kino_und_braucht_Knete_mit_Classes
 {
+    class Person
+    {
+        // Instanzvariablen
+        public string Name;
+        public double Geld; // in Euro
+
+        // Methoden (Aktionen)
+        public void BekommtGeld(double geld)
+        {
+            Console.WriteLine(Name + " bekommt " + geld + " Euro.");
+            Geld = Geld + geld;
+            // Geld += geld;
+            GibtAuskunft();
+        }
+
+        public void GibtGeldHer(double geld)
+        {
+            Console.WriteLine(Name + " gibt " + geld + " Euro her.");
+            Geld = Geld - geld;
+            // Geld -= geld;
+            GibtAuskunft();
+        }
+
+        public void GehtInsKino(double preis)
+        {
+            Console.WriteLine(Name + " kauft sich eine Kinokarte um " + preis + " Euro.");
+            GibtGeldHer(preis);
+            Console.WriteLine(Name + " schaut sich den Film an.");
+        }
+
+        public void GibtAuskunft()
+        {
+            Console.WriteLine(Name + " hat noch " + Geld + " Euro.");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            double preisDerKinoKarte = 12.5; // in Euro
-
-            Person roman = new Person();
-            roman.Name = "Roman";
-
             Console.Write("Wieviel Geld soll Roman anfangs besitzen? ");
-            roman.Geld = double.Parse(Console.ReadLine());
+            double anfangsbetrag = double.Parse(Console.ReadLine());
+
+            Person kind = new Person();
+            kind.Name = "Roman";
+            kind.Geld = anfangsbetrag;
+
+            Console.Write("Wieviel Geld soll Romans Mutter anfangs besitzen? ");
+            anfangsbetrag = double.Parse(Console.ReadLine());
 
             Person mutter = new Person();
             mutter.Name = "Mutter";
+            mutter.Geld = anfangsbetrag;
 
-            Console.Write("Wieviel Geld soll Romans Mutter anfangs besitzen? ");
-            mutter.Geld = double.Parse(Console.ReadLine());
+            kind.GibtAuskunft();
+            mutter.GibtAuskunft();
 
-            roman.Ausgeben();
-            mutter.Ausgeben();
+            Console.WriteLine("Hier beginnt die eigentliche Geschichte...\n");
 
-            Console.WriteLine("Die Geschichte beginnt...\n");
+            const double preisDerKinoKarte = 12.5; // in Euro
 
-            if (roman.Geld >= preisDerKinoKarte)
+            if (kind.Geld >= preisDerKinoKarte)
             {
-                roman.Kinobesuch(preisDerKinoKarte);
+                kind.GehtInsKino(preisDerKinoKarte);
             }
             else
             {
-                double fehlendesGeld = preisDerKinoKarte - roman.Geld;
-                Console.WriteLine(roman.Name + " fragt seine " + mutter.Name);
-                Console.WriteLine("Es fehlen " + fehlendesGeld + " Euro.");
+                double fehlendesGeld = preisDerKinoKarte - kind.Geld;
+                Console.WriteLine(kind.Name + " fragt " + mutter.Name + " um " + fehlendesGeld + " Euro.");
 
                 if (mutter.Geld >= fehlendesGeld)
-                {
+                {   // Mutter gibt Roman das fehlende Geld
                     mutter.GibtGeldHer(fehlendesGeld);
-                    roman.BekommtGeld(fehlendesGeld);
-                    roman.Kinobesuch(preisDerKinoKarte);
+                    kind.BekommtGeld(fehlendesGeld);
+                    kind.GehtInsKino(preisDerKinoKarte);
                 }
                 else
-                {
-                    Console.WriteLine(mutter.Name + " hat zuwenig Geld.");
-                    Console.WriteLine(roman.Name + " kann nicht ins Kino gehen und geht mit Kumpeln Fußballspielen.");
+                {   // Roman kann nicht ins Kino
+                    Console.WriteLine(kind.Name + " kann nicht ins Kino gehen.");
+                    Console.WriteLine(kind.Name + " geht mit den Kumpeln Fußballspielen.");
                 }
             }
 
